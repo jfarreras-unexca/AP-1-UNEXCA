a0083e56f4a26e26f637c5ee87fac834f30187be
Jose Ángel <jfarrerasunexca@gmail.com>
2024-11-15 18:48:06 -0400

    commit

--- ejercicios/jperez/suma_vectores.cpp
+++ ejercicios/jperez/suma_vectores.cpp
@@ -1,51 +1,17 @@
-/*#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main() {
-
-	int matriz1[3][3];
-
-	for ( int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-		matriz1[i][j] = i * j;
-	}
-}
-
-for ( int i = 0; i < 3; i++) {
-	
-	for (int j = 0; j < 3; j++) {
-		cout << matriz1[i][j] " ";
-	}
-	cout << " " << endl;
-
- }
-
-	return 0;
-} */
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Definir una matriz de 3 filas y 4 columnas
-    int matriz[3][3];
 
-    // Inicializar la matriz con valores
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            matriz[i][j] = i * j;
-        }
-    }
+	int matriz_1 [3] [3] = {
 
-    // Mostrar los valores de la matriz
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << matriz[i][j] << " ";
-        }
-        cout << endl;
-    }
+		{54, 22, 35}
+		{84, 32, 66}
+		{87, 63, 45}
+		
+};
 
-    return 0;
+
+	}
+
 }
