07e60713e37592d38bee7746da4af20a321f54e3
Jose Ángel <jfarrerasunexca@gmail.com>
2024-11-29 18:05:44 -0400

    commit

---
+++ talleres/jperez/calculoDecimales.cpp
@@ -0,0 +1,103 @@
+/*-------------------------------------------------
+
+Dados 10 numeros reales, determine Cuales tienen decimales
+
+Fecha:
+Catedra:
+NombredelPrograma:
+Lenguaje de Programacion:
+Programador:
+Cedula:
+Descripcion del Programa:
+(coloco aqui el enumciado del ejercicio de la guia)
+
+Comentarios:
+*/
+
+
+
+
+// LIBRERIAS o Directivas del preprocesador
+//EJEMPLO
+/*#include <stdlib.h>
+#include <math.h>
+#include <string>
+//#include <windows.h>
+#include <time.h>*/
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+
+
+
+
+using namespace std;
+/*======================================================
+// VARIABLES GLOBALES
+//======================================================*/
+
+
+//======================================================
+
+
+
+/*=======================================================
+// PROTOTIPOS //
+//=======================================================*/
+//------------------------------------------
+/*coloca aqui Los prototipos si colocas
+ los prototipos las funciones deben
+ ir despues de la funcion main
+
+
+
+
+*/
+/*=======================================================
+// FUNCIONES //
+//=======================================================*/
+
+void limpiarconsola();
+
+
+//------------------------------------------
+//funcion coloca aqui tus funciones
+//o Los prototipos si colocas los prototipos las funciones deben ir despues de la funcion main
+
+
+void limpiarconsola() {
+    #ifdef _WIN32
+        system("cls"); /* WINDOWS */
+    #else
+        std::system("clear"); /* LINUX */
+    #endif
+}
+
+
+//======================================================
+// FUNCION PRINCIPAL
+//======================================================
+
+
+main()
+{
+    limpiarconsola();
+    cout << "**********************************" << endl << endl;
+    cout << "     Se Compilo sin Errores       " << endl << endl;
+    cout << "**********************************" << endl;
+    return 0;
+}
+/*=======================================================
+// FUNCIONES //
+//=======================================================*/
+//------------------------------------------
+//funcion coloca aqui tus funciones si colocaste los prototipos las funciones deben ir despues de la funcion main
+
+
+//----------------------------------
+// Bloque de instrucciones
+//----------------------------------
+// funcion crea un numero aleatorio
